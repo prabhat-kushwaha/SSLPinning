f240126754d9cd00b902db94eb9c7fc9851ce3e8
prabhat <prabhatkushwaha7575@gmail.com>
2022-05-21 12:08:37 +0530

    Initial commit

---
+++ app/src/main/java/com/prabhatkushwaha/sslpinning/GithubUser.kt
@@ -0,0 +1,30 @@
+package com.prabhatkushwaha.sslpinning
+
+import com.google.gson.annotations.SerializedName
+
+
+data class GithubUser(
+
+    @SerializedName("login")
+    var login: String,
+    @SerializedName("avatar_url")
+    var avatarUrl: String,
+    @SerializedName("name")
+    var name: String,
+    @SerializedName("bio")
+    var bio: String,
+    @SerializedName("public_repos")
+    var publicRepos: Int,
+    @SerializedName("followers")
+    var followers: Int,
+    @SerializedName("following")
+    var following: Int
+) {
+    fun getFollowers(): String {
+        return "Followers " + followers
+    }
+
+    fun getFollowing(): String {
+        return "Following " + following
+    }
+}
---
+++ app/src/main/java/com/prabhatkushwaha/sslpinning/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.prabhatkushwaha.sslpinning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import okhttp3.CertificatePinner
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /**
+         * use this website to find cert public key by domain name
+         * https://approov.io/tools/static-pinning/
+         */
+        val certificatePin = CertificatePinner.Builder()
+            .add("api.github.com", "sha256/uyPYgclc5Jt69vKu92vci6etcBDY8UNTyrHQZJpVoZY")
+            .build()
+
+        /**
+         * with valid cert public key api provide valid user data
+         * in case of invalid cert it show Certificate pinning failure exception
+         */
+
+        val okHttpClient = OkHttpClient.Builder()
+            .certificatePinner(certificatePin)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.github.com/")
+            .client(okHttpClient)
+            .build().create(MyApi::class.java)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val data = retrofit.getUserData()
+            data.enqueue(object : Callback<GithubUser> {
+                override fun onResponse(
+                    call: Call<GithubUser>,
+                    response: retrofit2.Response<GithubUser>
+                ) {
+                    Log.d(TAG, "onResponse: ${response.body()}")
+                }
+
+                override fun onFailure(call: Call<GithubUser>, t: Throwable) {
+                    Log.d(TAG, "onFailure: ${t.localizedMessage}")
+                }
+            })
+            Log.d(TAG, "onCreate: $data")
+
+        }
+    }
+
+
+    interface MyApi {
+        @GET("/users/prabhat-kushwaha")
+        fun getUserData():
+                Call<GithubUser>
+    }
+
+}
